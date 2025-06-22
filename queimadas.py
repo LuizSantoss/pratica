28858aaabe2b023a360981685ce99de3c115fed3
luizSantoss <lzfelipepro@outlook.com>
2025-06-21 22:50:09 -0300

    gerando gráficos de queimada

--- queimadas.py
+++ queimadas.py
@@ -12,3 +12,15 @@ print("\n\n")
 #dados subsituidos pela media
 df_tratado = df.fillna(df["Porcentagem_Queimadas"].mean())
 print(df_tratado)
+
+for estado in df_tratado["Estado"].unique():
+    dados_estado = df_tratado[df_tratado["Estado"] == estado]
+    plt.plot(dados_estado["Ano"], dados_estado["Porcentagem_Queimadas"], marker='o', label=estado)
+
+plt.title("Porcentagem de Queiamadas no Brasil ")
+plt.xlabel("Ano")
+plt.ylabel("Porcentagem de Queimadas")
+plt.legend(title="Estado")
+plt.grid(True)
+plt.tight_layout()
+plt.show()
