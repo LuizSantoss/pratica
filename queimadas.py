bb0d85829ccf13038d8a5c73efd8e3688e93f2a7
luizSantoss <lzfelipepro@outlook.com>
2025-06-21 21:06:49 -0300

    tratando dados vazios

--- queimadas.py
+++ queimadas.py
@@ -4,3 +4,11 @@ import matplotlib.pyplot as plt
 df = pd.read_csv("queimadas.csv")
 print(df['Porcentagem_Queimadas'].mean())
 
+#visualizando onde falta dados (True)
+print(df.isna())
+
+print("\n\n")
+
+#dados subsituidos pela media
+df_tratado = df.fillna(df["Porcentagem_Queimadas"].mean())
+print(df_tratado)
